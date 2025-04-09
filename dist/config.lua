815f003e887b105fccd335785ed33dc6df1464d7
Касьянов <info@carono.ru>
2025-04-10 00:57:14 +0500

    refs

--- dist/config.lua
+++ dist/config.lua
@@ -2,6 +2,7 @@
 require("os")
 
 game_path = os.getenv('LOCALAPPDATA') .. "\\TheLastWar\\Launch.exe"
+username = os.getenv('username')
 logout_timeout = 15 * 60 * 1000
 check_queue_timeout = 1 * 60 * 1000
 dalay_after_check = 3 * 60 * 1000
@@ -9,7 +10,7 @@ win_pos_x = 0
 win_pos_y = 0
 
 storageLib = require [[lib/storage]]
-
+configFile = username .. ".config.env"
 config = {
     game_path = game_path,
     logout_timeout = logout_timeout,
@@ -19,8 +20,8 @@ config = {
     win_pos_y = win_pos_y,
 }
 
-if (fileexists([["config.env"]]) == "0") then
-    storageLib.save("config.env", config)
+if (fileexists(configFile) == "0") then
+    storageLib.save(configFile, config)
 end
 
-config = storageLib.load("config.env")
+config = storageLib.load(configFile)
